24d4f6e7aa4c48eccb9c29d177127d1264ce39d3
AhmedMounir001 <AMounir@path-solutions.com>
2020-04-15 16:21:30 +0200

    Update package deleted from other branch

---
+++ src/main/java/mokhtar/testRepoZZZ/Test1504.java
@@ -0,0 +1,11 @@
+package mokhtar.testRepoZZZ;
+
+import org.junit.Test;
+
+public class Test1504 {
+
+	@Test
+	public void testnewMethod() {
+		System.out.println("New updates at 15-04-2020");
+	}
+}
